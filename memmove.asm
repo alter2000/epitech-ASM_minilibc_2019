4d96c1b9a8e89ec5f037b010e56093b403fe4234
Jor Bashllari <altnate2000@gmail.com>
2020-03-01 23:37:15 +0100

    add memmove

---
+++ memmove.asm
@@ -0,0 +1,30 @@
+global memmove:function
+
+memmove:
+	push rbp
+	mov rbp, rsp
+	mov rax, rdx
+	sub rax, 1
+	mov r9, -1
+	mov r10, -1
+	jmp .cond
+
+.cond:
+	cmp rdi, rsi
+	jg .loop_in
+	xor rax, rax
+	mov r9, 1
+	mov r10, rdx
+
+.loop_in:
+	cmp rax, r10
+	jz .end
+	mov r8b, [rsi + rax]
+	mov [rdi + rax], r8b
+	add rax, r9
+	jmp .loop_in
+
+.end:
+	mov rax, rdi
+	leave
+	ret
